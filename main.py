31ae2dd2f4a15ea5ed09595ea6d835f48860961e
AstrotrekGithub <85321323+AstrotrekGithub@users.noreply.github.com>
2022-11-07 12:16:47 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+from websocket import create_connection
+import json, time
+import requests
+import random
+import cloudscraper
 
+scraper = cloudscraper.create_scraper(
+    browser={
+        'custom': 'ScraperBot/1.0',
+    }
+)
